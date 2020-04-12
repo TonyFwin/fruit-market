c34d1a1d89cb29a78db714b1f7d8b0e533d0c150
Tony Nguyen <tonynguyen201@gmail.com>
2020-04-11 22:49:36 -0500

    refactored to more proper implementation

--- src/base.js
+++ src/base.js
@@ -1,14 +1,19 @@
 import Rebase from 're-base';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 
-const firebaseApp = firebase.initializeApp({
+const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL
-});
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
 
-const base = Rebase.createClass(firebaseApp.database());
-
-export { firebaseApp };
+const app = firebase.initializeApp(config);
+const base = Rebase.createClass(app.database());
 
 export default base;
